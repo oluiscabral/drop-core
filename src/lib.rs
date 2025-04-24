f830c0e5244c75698d967199cacc00e0a044efc9
oluiscabral <oluiscabral@hotmail.com>
2025-04-24 12:49:46 -0300

    WIP - connection handler

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,18 @@
-use iroh::{protocol::Router, Endpoint};
-use iroh_blobs::{
-    net_protocol::Blobs, rpc::client::blobs::AddOutcome, store::mem::Store, ticket::BlobTicket,
-    util::SetTagOption, BlobFormat,
+use std::{
+    future::Future,
+    pin::Pin,
+    sync::RwLock,
+    time::{Duration, Instant},
 };
+
+use iroh::{
+    protocol::{ProtocolHandler, Router},
+    Endpoint,
+};
+
+use anyhow::Result;
+use iroh_base::ticket::NodeTicket;
+use rand::Rng;
 
 mod entities;
 
@@ -28,189 +38,173 @@ pub enum Error {
     RetrieveCollectionError,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct SendFilesRequest {
     pub files: Vec<FilePayload>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct FilePayload {
     pub name: String,
     pub data: Vec<u8>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct SendFilesResponse {
-    pub ticket: TicketOutput,
+    pub ticket: String,
+    pub confirmation: u8,
 }
 
-#[derive(Debug)]
-pub struct TicketOutput {
-    pub id: String,
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ReceiveFilesRequest {
-    pub ticket: TicketPayload,
+    pub ticket: String,
+    pub confirmation: u8,
 }
 
-#[derive(Debug)]
-pub struct TicketPayload {
-    pub id: String,
-}
-
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct ReceiveFilesResponse {
-    pub bag: BagOutput,
-}
-
-#[derive(Debug)]
-pub struct BagOutput {
-    pub ticket: TicketOutput,
     pub files: Vec<FileOutput>,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 pub struct FileOutput {
     pub id: String,
     pub name: String,
     pub data: Vec<u8>,
 }
 
+#[derive(Debug)]
+struct SendFilesHandler {
+    request: SendFilesRequest,
+}
+
+impl ProtocolHandler for SendFilesHandler {
+    fn on_connecting(
+        &self,
+        connecting: iroh::endpoint::Connecting,
+    ) -> Pin<Box<dyn Future<Output = Result<iroh::endpoint::Connection>> + Send + 'static>> {
+        return Box::pin(async move {
+            let conn = connecting.await?;
+            Ok(conn)
+        });
+    }
+
+    fn shutdown(&self) -> Pin<Box<dyn Future<Output = ()> + Send + 'static>> {
+        return Box::pin(async {});
+    }
+
+    fn accept(
+        &self,
+        connection: iroh::endpoint::Connection,
+    ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>> {
+        println!(
+            "SENDING FILES TO '{}'",
+            connection.remote_node_id().unwrap()
+        );
+        return Box::pin(async { Ok(()) });
+    }
+}
+
+struct SendFilesConfiguration {
+    pub router: Router,
+    pub expires_at: Instant,
+}
+
 pub struct Drop {
-    blobs: Blobs<Store>,
-    router: Router,
+    send_files_configurations: RwLock<Vec<SendFilesConfiguration>>,
 }
 
 impl Drop {
-    pub async fn new() -> Result<Self, Error> {
+    pub fn new() -> Result<Self, Error> {
+        return Ok(Self {
+            send_files_configurations: RwLock::new(Vec::new()),
+        });
+    }
+
+    pub async fn send_files(&self, request: SendFilesRequest) -> Result<SendFilesResponse, Error> {
+        /*
+           TODO:
+               - Limit how many send files configs is possible to exist at the same time
+               - Maybe let the API client decide the expiration time
+        */
+        let confirmation: u8 = rand::rng().random_range(0..=99);
+
         let endpoint = Endpoint::builder()
             .discovery_n0()
             .bind()
             .await
             .map_err(|e| {
                 eprintln!("{}", e.to_string());
+                // TODO: REVIEW ERROR NAME
                 return Error::InitializeEnvironmentError;
             })?;
-        let blobs = Blobs::memory().build(&endpoint);
+        let node_addr = endpoint.node_addr().await.map_err(|e| {
+            eprintln!("{}", e.to_string());
+            // TODO: REVIEW ERROR NAME
+            return Error::InitializeEnvironmentError;
+        })?;
+
+        let handler = SendFilesHandler {
+            request: request.clone(),
+        };
         let router = Router::builder(endpoint)
-            .accept(iroh_blobs::ALPN, blobs.clone())
+            .accept([confirmation], handler)
             .spawn()
             .await
             .map_err(|e| {
                 eprintln!("{}", e.to_string());
                 return Error::InitializeEnvironmentError;
             })?;
-        return Ok(Self { blobs, router });
-    }
-
-    async fn send_files(&self, request: SendFilesRequest) -> Result<SendFilesResponse, Error> {
-        let node_id = self.router.endpoint().node_id();
-
-        let mut outcomes: Vec<AddOutcome> = vec![];
-        for file in &request.files {
-            let outcome = self
-                .blobs
-                .client()
-                .add_bytes(file.data.clone())
-                .await
-                .map_err(|e| {
-                    eprintln!("{}", e.to_string());
-                    return Error::ReadFileDataError(file.name.clone());
-                })?;
-            outcomes.push(outcome);
-        }
-
-        let collection = outcomes
-            .iter()
-            .zip(request.files.iter())
-            .map(|(outcome, file)| (file.name.clone(), outcome.hash))
-            .collect();
-        let collection_outcome = self
-            .blobs
-            .client()
-            .create_collection(collection, SetTagOption::Auto, Default::default())
-            .await
-            .map_err(|e| {
-                eprintln!("{}", e.to_string());
-                return Error::CreateCollectionError;
-            })?;
-
-        let ticket = BlobTicket::new(node_id.into(), collection_outcome.0, BlobFormat::HashSeq)
-            .map_err(|e| {
-                eprintln!("{}", e.to_string());
-                return Error::CreateTicketError;
-            })?;
+        let config = SendFilesConfiguration {
+            router,
+            expires_at: Instant::now() + Duration::from_secs(60 * 15),
+        };
+        self.send_files_configurations.write().unwrap().push(config);
 
         return Ok(SendFilesResponse {
-            ticket: TicketOutput {
-                id: ticket.to_string(),
-            },
+            ticket: NodeTicket::new(node_addr).to_string(),
+            confirmation,
         });
     }
 
-    async fn receive_files(
+    pub async fn receive_files(
         &self,
         request: ReceiveFilesRequest,
     ) -> Result<ReceiveFilesResponse, Error> {
-        let ticket: BlobTicket = request.ticket.id.parse().map_err(|_| {
+        /*
+           TODO:
+               - Should not allow receiving files from itself
+        */
+        let ticket: NodeTicket = request.ticket.parse().map_err(|_| {
             return Error::ParseTicketError;
         })?;
-        if ticket.format() != BlobFormat::HashSeq {
-            return Err(Error::InvalidTicketFormatError);
-        }
 
-        self.blobs
-            .client()
-            .download_hash_seq(ticket.hash(), ticket.node_addr().clone())
+        let endpoint = Endpoint::builder()
+            .discovery_n0()
+            .bind()
             .await
             .map_err(|e| {
                 eprintln!("{}", e.to_string());
+                // TODO: REVIEW ERROR NAME
+                return Error::InitializeEnvironmentError;
+            })?;
+
+        let connection = endpoint
+            .connect(ticket, &[request.confirmation])
+            .await
+            .map_err(|e| {
+                eprintln!("{}", e.to_string());
+                // TODO: UPDATE ERROR NAME
                 return Error::InitializeDownloadError;
-            })?
-            .await
-            .map_err(|e| {
-                eprintln!("{}", e.to_string());
-                return Error::FinishDownloadError;
             })?;
 
-        let collection = self
-            .blobs
-            .client()
-            .get_collection(ticket.hash())
-            .await
-            .map_err(|e| {
-                eprintln!("{}", e.to_string());
-                return Error::RetrieveCollectionError;
-            })?;
+        println!(
+            "RECEIVING FILES FROM '{}'",
+            connection.remote_node_id().unwrap()
+        );
 
-        let mut files: Vec<FileOutput> = vec![];
-        for (name, hash) in collection.iter() {
-            let data = self
-                .blobs
-                .client()
-                .read_to_bytes(hash.clone())
-                .await
-                .map_err(|e| {
-                    eprintln!("{}", e.to_string());
-                    return Error::ReadFileDataError(name.clone());
-                })?;
-            let file = FileOutput {
-                id: hash.to_string(),
-                name: name.clone(),
-                data: data.to_vec(),
-            };
-            files.push(file);
-        }
-
-        return Ok(ReceiveFilesResponse {
-            bag: BagOutput {
-                files,
-                ticket: TicketOutput {
-                    id: request.ticket.id,
-                },
-            },
-        });
+        // TODO
+        return Ok(ReceiveFilesResponse { files: Vec::new() });
     }
 }
 
