16810be67199355108bd15a47a4bdb29745a437c
oluiscabral <oluiscabral@hotmail.com>
2025-04-28 13:35:43 -0300

    WIP - sends handshake and get projections uniformly

--- src/lib.rs
+++ src/lib.rs
@@ -97,6 +97,30 @@ struct FileProjection {
     pub file: FileDetails,
 }
 
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct SendFilesHandshake {
+    sender: Profile,
+    files: Vec<FileMetadata>,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct Profile {
+    name: String,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct FileMetadata {
+    id: String,
+    name: String,
+    len: u64,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize)]
+struct FileProjection2 {
+    id: String,
+    data: Vec<u8>,
+}
+
 #[derive(Debug)]
 struct SendFilesHandler {
     expires_at: Instant,
@@ -129,11 +153,6 @@ impl ProtocolHandler for SendFilesHandler {
         &self,
         connecting: iroh::endpoint::Connecting,
     ) -> Pin<Box<dyn Future<Output = Result<iroh::endpoint::Connection>> + Send + 'static>> {
-        self.subscribers.iter().for_each(|s| {
-            s.notify(SendFilesEvent::Connecting {
-                // TODO
-            });
-        });
         if Instant::now() > self.expires_at {
             return Box::pin(async {
                 return Err(anyhow::anyhow!("Connection handler has expired."));
@@ -157,11 +176,6 @@ impl ProtocolHandler for SendFilesHandler {
     }
 
     fn shutdown(&self) -> Pin<Box<dyn Future<Output = ()> + Send + 'static>> {
-        self.subscribers.iter().for_each(|s| {
-            s.notify(SendFilesEvent::Closed {
-                // TODO
-            });
-        });
         return Box::pin(async {});
     }
 
@@ -171,60 +185,57 @@ impl ProtocolHandler for SendFilesHandler {
     ) -> Pin<Box<dyn Future<Output = Result<()>> + Send + 'static>> {
         let has_finished = self.has_finished.clone();
         let request = self.request.clone();
-        let subscribers = self.subscribers.clone();
+        // let subscribers = self.subscribers.clone();
         return Box::pin(async move {
-            println!("EXECUTING THE CONNECTION ACCEPTED HANDLER");
             let mut bi = connection.accept_bi().await?;
-            println!("ACCEPTED BI!");
-            subscribers.iter().for_each(|subscriber| {
-                subscriber.notify(SendFilesEvent::Connected {
-                                // TODO
-                            });
-            });
-            // TODO: RECEIVES THEIR PROFILE AND NOTIFY SUBSCRIBERS
-            // TODO: SENDS MY PROFILE
-            for f in &request.files {
-                // TODO: SENDS FILE IN SMALL PACKETS, WITH A HEADER CONTAINING THEIR METADATA
-                let id = Uuid::new_v4().to_string();
-                let data: Vec<u8> = f.data.clone();
-                let total: u64 = data.len().try_into().unwrap();
-                let chunk_len = 1024; // TODO: flexibilize chunk size
+            // TODO: SEND MY PROFILE
+            let identified_files_iter = request
+                .files
+                .iter()
+                .map(|f| return (Uuid::new_v4().to_string(), f));
+            let my_handshake = SendFilesHandshake {
+                sender: Profile {
+                    name: String::from("TODO: PROFILE SYSTEM"),
+                },
+                files: identified_files_iter
+                    .clone()
+                    .map(|idf| {
+                        return FileMetadata {
+                            id: idf.0.clone(),
+                            len: idf.1.data.len() as u64,
+                            name: idf.1.name.clone(),
+                        };
+                    })
+                    .collect(),
+            };
+            let my_handshake_serialized = serde_json::to_vec(&my_handshake).unwrap();
+            // TODO: LIMIT HOW MUCH FILES IT IS POSSIBLE TO SEND
+            let my_handshake_serialized_len = my_handshake_serialized.len() as u32;
+            let my_handshake_serialized_len_bytes = my_handshake_serialized_len.to_be_bytes();
+            // NOTIFY RECEIVER ABOUT THE HANDSHAKE LENGTH
+            bi.0.write_all(&my_handshake_serialized_len_bytes).await?;
+            // SEND HANDSHAKE
+            bi.0.write_all(&my_handshake_serialized).await?;
 
-                let mut offset = 0;
-                for chunk in data.chunks(chunk_len) {
-                    let len = chunk.len() as u64;
-                    let end = offset + len;
+            for (id, f) in identified_files_iter {
+                let chunk_len = 1024; // TODO: FLEXIBILIZE CHUNK LEN
 
-                    let projection = FileProjection {
+                for chunk in f.data.chunks(chunk_len) {
+                    let projection = FileProjection2 {
                         id: id.clone(),
-                        total,
-                        offset,
                         data: chunk.to_vec(),
-                        file: FileDetails {
-                            name: f.name.clone(),
-                        },
                     };
                     let serialized_projection = serde_json::to_vec(&projection).unwrap();
-                    let serialized_projection_bytes =
-                        uniffi::deps::bytes::Bytes::copy_from_slice(&serialized_projection);
-
-                    subscribers.iter().for_each(|subscriber| {
-                        subscriber.notify(SendFilesEvent::Sending {
-                                        // TODO
-                                    });
-                    });
-
-                    bi.0.write_chunk(serialized_projection_bytes).await?;
-
-                    subscribers.iter().for_each(|subscriber| {
-                        subscriber.notify(SendFilesEvent::Sent {
-                                    // TODO
-                                });
-                    });
-
-                    offset = end;
+                    // TODO: LIMIT CHUNK LEN **NEAR** U16, BECAUSE JSON TAKES MORE SPACE
+                    let serialized_projection_len = serialized_projection.len() as u16;
+                    let serialized_projection_len_bytes = serialized_projection_len.to_be_bytes();
+                    // NOTIFY RECEIVER ABOUT THE HANDSHAKE LENGTH
+                    bi.0.write_all(&serialized_projection_len_bytes).await?;
+                    // SEND PROJECTION
+                    bi.0.write_all(&serialized_projection).await?;
                 }
             }
+
             bi.0.finish()?;
             bi.0.stopped().await?;
             connection.close(iroh::endpoint::VarInt::from_u32(0), &[0]);
@@ -431,36 +442,78 @@ impl Drop {
             .await
             .map_err(|e| Error::CreateBidirectionalCommunicationError(e.to_string()))?;
 
+        // TODO: SEND MY HANDSHAKE
         bi.0.write_all(&[0])
             .await
             .map_err(|e| Error::ActivateBidirectionalCommunicationError(e.to_string()))?;
         bi.0.finish()
             .map_err(|e| Error::FinishCommunicationInputError(e.to_string()))?;
 
-        // TODO: SEND MY PROFILE
-        // TODO: RECEIVES THEIR PROFILE (AND POSSIBLY CHUNK SIZE??)
-        let chunk_len = 1024; // TODO: flexibilize chunk size
+        let maybe_their_handshake_len_raw =
+            bi.1.read_chunk(4, true)
+                .await
+                .map_err(|e| Error::ReadCommunicationOutputError(e.to_string()))?;
+
+        if maybe_their_handshake_len_raw.is_none() {
+            // TODO: CREATE ERROR CODE?
+            // TODO: UPDATE map_err WITH PROPER ERROR NAME
+            bi.1.stop(iroh::endpoint::VarInt::from_u32(0))
+                .map_err(|e| Error::ReadCommunicationOutputError(e.to_string()))?;
+            endpoint.close().await;
+            return Err(Error::ReadCommunicationOutputError(String::from(
+                "TODO: UPDATE ERROR NAME",
+            )));
+        }
+
+        let their_handshake_len_raw = maybe_their_handshake_len_raw.unwrap();
+        let their_handshake_len_bytes: [u8; 4] =
+            their_handshake_len_raw.bytes[0..4].try_into().unwrap();
+        let their_handshake_len: u32 = u32::from_be_bytes(their_handshake_len_bytes);
+
+        let maybe_their_handshake_raw =
+            bi.1.read_chunk(their_handshake_len as usize, true)
+                .await
+                .map_err(|e| Error::ReadCommunicationOutputError(e.to_string()))?;
+
+        if maybe_their_handshake_raw.is_none() {
+            // TODO: CREATE ERROR CODE?
+            // TODO: UPDATE map_err WITH PROPER ERROR NAME
+            bi.1.stop(iroh::endpoint::VarInt::from_u32(0))
+                .map_err(|e| Error::ReadCommunicationOutputError(e.to_string()))?;
+            endpoint.close().await;
+            return Err(Error::ReadCommunicationOutputError(String::from(
+                "TODO: UPDATE ERROR NAME",
+            )));
+        }
+
+        let their_handshake_raw = maybe_their_handshake_raw.unwrap();
+        // TODO: HANDLE ERRORS
+        let their_handshake: SendFilesHandshake =
+            serde_json::from_slice(&their_handshake_raw.bytes).unwrap();
+
         let mut files: Vec<FileOutput> = Vec::new();
         loop {
-            let maybe_chunk =
-                bi.1.read_chunk(chunk_len, true)
+            let maybe_chunk_len_raw =
+                bi.1.read_chunk(2, true)
                     .await
                     .map_err(|e| Error::ReadCommunicationOutputError(e.to_string()))?;
-            if maybe_chunk.is_none() {
+            if maybe_chunk_len_raw.is_none() {
                 break;
             }
-            let chunk = maybe_chunk.unwrap();
-            let projection: FileProjection = serde_json::from_slice(&chunk.bytes)
+            let chunk_len_raw = maybe_chunk_len_raw.unwrap();
+            let chunk_len_bytes: [u8; 2] = chunk_len_raw.bytes[0..2].try_into().unwrap();
+            let chunk_len: u16 = u16::from_be_bytes(chunk_len_bytes);
+
+            let maybe_chunk_raw =
+                bi.1.read_chunk(chunk_len as usize, true)
+                    .await
+                    .map_err(|e| Error::ReadCommunicationOutputError(e.to_string()))?;
+            if maybe_chunk_raw.is_none() {
+                break;
+            }
+            let chunk_raw = maybe_chunk_raw.unwrap();
+            let projection: FileProjection2 = serde_json::from_slice(&chunk_raw.bytes)
                 .map_err(|e| Error::DeserializeFileProjectionError(e.to_string()))?;
-            self.receive_files_subscribers
-                .read()
-                .await
-                .iter()
-                .for_each(|s| {
-                    s.notify(ReceiveFilesEvent::RECEIVING {
-                    // TODO
-                });
-                });
 
             let maybe_file = files.iter_mut().find(|f| f.id == projection.id);
             if maybe_file.is_some() {
@@ -469,14 +522,14 @@ impl Drop {
             } else {
                 let file = FileOutput {
                     id: projection.id.clone(),
-                    name: projection.file.name,
+                    name: String::from("TODO: GET FILE NAME"),
                     data: projection.data.clone(),
                 };
                 files.push(file);
             }
         }
-        endpoint.close().await;
 
+        endpoint.close().await;
         return Ok(ReceiveFilesResponse { files });
     }
 }
